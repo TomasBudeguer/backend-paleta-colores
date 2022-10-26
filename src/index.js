cb499380d13878008e0142aa621e2057d4d71089
Tomas <tomibudeguer20@gmail.com>
2022-10-26 17:02:58 -0300

    agregue los mddlewares necesarios

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
-console.log("hola mundo");
+import morgan from "morgan";
+import cors from 'cors'
+import path from 'path'
 
 const app = express();
 
@@ -8,3 +10,16 @@ app.set("port", process.env.PORT || 4000);
 app.listen(app.get("port"), () => {
   console.log("Estoy en el puerto " + app.get("port"));
 });
+
+// MIDDLEWARES:
+app.use(morgan('dev'))
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, '../public')))
+
+// RUTAS:
+// http://localhost:4000/
+app.get('/colores', (req, res)=>{
+    res.send('Esto es una prueba de la peticion GET de colores')
+})
