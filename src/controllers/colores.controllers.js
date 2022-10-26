1717b3b1da93ca7e1b6bdf859c3c9c879e264436
Tomas <tomibudeguer20@gmail.com>
2022-10-26 18:33:55 -0300

    hice la peticion GET para obtener todos los colores

--- src/controllers/colores.controllers.js
+++ src/controllers/colores.controllers.js
@@ -1,7 +1,15 @@
 import Color from "../models/color";
 
-export const listarColores = (req, res) => {
-  res.send("Esto es una prueba de la peticion GET de colores");
+export const listarColores = async (req, res) => {
+  try {
+    const listaColores = await Color.find();
+    res.status(200).json(listaColores);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error al buscar los colores",
+    });
+  }
 };
 export const crearColor = async (req, res) => {
   try {
