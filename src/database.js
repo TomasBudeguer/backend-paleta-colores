d027f40646849bc2eb3f10fdcbdc2c1aa300243d
Tomas <tomibudeguer20@gmail.com>
2022-11-04 01:03:26 -0300

    agregue el archivo vercel y la base de mongo atlas

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-const url = "mongodb://localhost:27017/colores";
+// const url = "mongodb://localhost:27017/colores"; //bd local
+const url = "mongodb+srv://TomasBudeguer:KJI5kQLnLz619pLl@cluster0.vjacqdu.mongodb.net/paleta-colores";
 
 mongoose.connect(url);
 
